993cc5e0c2775cd9e5071b0cd38c8cd5688012c6
Imeth Pathirana <imethp2@gmail.com>
2023-11-21 00:35:25 +0530

    Create vite react

--- SDGP/src/App.jsx
+++ SDGP/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
-
+import SignIn from './components/SignIn/SignIn.jsx'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-      <p>You clicked {count} times.</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-    </div>
+    // <div className="App">
+    //   <h1>hi !</h1>
+    //   <p>You clicked {count} times.</p>
+    //   <button onClick={() => setCount(count + 1)}>Click me</button>
+    // </div>
+    <SignIn />
   )
 }
 
