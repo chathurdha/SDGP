e26fcb3ae763593f349fa866f320922bb3aa62ec
Randila Premarathne <randilamenukapremarathne@gmail.com>
2024-02-11 12:48:34 +0530

    Add volunteer signin component

---
+++ Frontend/src/pages/SignInBackground.jsx
@@ -0,0 +1,9 @@
+import VolunteerSignIn from "../components/VolunteerSignIn.jsx";
+
+export default function SignInBackground() {
+    return (
+        <div className='grid place-items-center w-screen h-screen'>
+            <VolunteerSignIn />
+        </div>
+    )
+}
