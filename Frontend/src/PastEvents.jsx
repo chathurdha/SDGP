3daf202c3dfb60d710d6c1ec6088feac7bbb3454
IsuruGimhana <145018121+IsuruGimhana@users.noreply.github.com>
2024-02-12 23:57:30 +0530

    seminar description card

--- Frontend/src/PastEvents.jsx
+++ Frontend/src/PastEvents.jsx
@@ -1,23 +1,48 @@
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
+
 import React from 'react';
-import { useState } from 'react';
+import Card from './components/card';
 
 const PastEvents = () => {
 
-    const organizations = ["Organization1","Oragization2","Organization3"];
-    const [org,setOrg] = useState(organizations[0]);
-    const handleEvent = (e) => {
-        setOrg(e.target.value);
-    };
+  const seminars = [
+    {_id: 1, name: "Seminar 1", description: "This is the first seminar"}, 
+    {_id: 2, name: "Seminar 2", description: "This is the second seminar"}, 
+    {_id: 3, name: "Seminar 3", description: "This is the third seminar"}
+  ];
 
-    return ( 
-        <select value={org} onChange={handleEvent}>
-            {organizations.map((org) => (
-                <option value={org}>
-                    {org}
-                </option>
-            ))}
-        </select>
-     );
+  // const [seminars, setSeminars] = useState(null);
+
+  // useEffect(() => {
+
+  //   const fetchData = async () => {
+  //     const response = await fetch('api/seminars');
+  //     const json = await response.json();
+  //     console.log(json);
+
+  //     if (response.ok) {
+  //       setSeminars(json);
+  //     }
+
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+    return (
+      <div className='container mx-auto p-4'>
+        <h1 className='text-2xl font-bold mb-4'>Past Events</h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+          {/* <img src="./images/1" alt="" /> */}
+          {seminars && seminars.map((seminar) => (
+            // <card key={data._id} name={data.name} image={data._id} address={data.address} />
+            <Card key={seminar._id} seminar={seminar} />
+            // <p key={seminar._id}>{seminar.name}</p>
+          ))}
+        </div>
+      </div>
+    );
 }
  
 export default PastEvents;
