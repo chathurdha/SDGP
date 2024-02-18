eb4927a4d3c7dfee71e5d8d1a0c6e004b81e22f2
IsuruGimhana <145018121+IsuruGimhana@users.noreply.github.com>
2024-02-19 02:54:45 +0530

    Filter option implemented(not finalized)

--- Frontend/src/PastEvents.jsx
+++ Frontend/src/PastEvents.jsx
@@ -4,12 +4,20 @@
 import React from 'react';
 import Card from './components/card';
 
+import FilterOption from './components/FilterOption';
+
 const PastEvents = () => {
 
   const seminars = [
-    {_id: 1, name: "Seminar 1", description: "This is the first seminar", rating: 4.0}, 
-    {_id: 2, name: "Seminar 2", description: "This is the second seminar", rating: 3.0}, 
-    {_id: 3, name: "Seminar 3", description: "This is the third seminar", rating: 3.5}
+    {_id: 1, name: "Seminar 1", description: "This is the first seminar", rating: 4.0, organizationId: 1}, 
+    {_id: 2, name: "Seminar 2", description: "This is the second seminar", rating: 3.0, organizationId: 2}, 
+    {_id: 3, name: "Seminar 3", description: "This is the third seminar", rating: 3.5, organizationId: 3}
+  ];
+
+  const organizations = [
+    {_id: 1, name: "Organization 1", description: "This is the first organization"}, 
+    {_id: 2, name: "Organization 2", description: "This is the second organization"}, 
+    {_id: 3, name: "Organization 3", description: "This is the third organization"}
   ];
 
   // const [seminars, setSeminars] = useState(null);
@@ -30,19 +38,20 @@ const PastEvents = () => {
   //   fetchData();
   // }, []);
 
-    return (
-      <div className='w-full'>
-        <h1 className=''>Past Events</h1>
-        <div className=''>
-          {/* <img src="./images/1" alt="" /> */}
-          {seminars && seminars.map((seminar) => (
-            // <card key={data._id} name={data.name} image={data._id} address={data.address} />
-            <Card key={seminar._id} seminar={seminar} />
-            // <p key={seminar._id}>{seminar.name}</p>
-          ))}
-        </div>
+  return (
+    <div className='w-full'>
+      <h1 className=''>Past Events</h1>
+      <FilterOption />
+      <div className=''>
+        {/* <img src="./images/1" alt="" /> */}
+        {/* {seminars && seminars.map((seminar) => (
+          // <card key={data._id} name={data.name} image={data._id} address={data.address} />
+          <Card key={seminar._id} seminar={seminar} />
+          // <p key={seminar._id}>{seminar.name}</p>
+        ))} */}
       </div>
-    );
+    </div>
+  );
 }
  
 export default PastEvents;
