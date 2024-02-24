7157bc13ee1946d4986189920aaafd8188b05173
IsuruGimhana <145018121+IsuruGimhana@users.noreply.github.com>
2024-02-24 05:34:53 +0530

    Past events design implemented partially

---
+++ Frontend/src/components/MatchingSeminars.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from './Card';
+
+const MatchingSeminars = ({ seminars }) => {
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
+      {seminars &&
+        seminars.map((seminar) => (
+          <Card
+            key={seminar._id}
+            seminar={seminar}
+            className="w-full" // Added w-full and border class
+          />
+        ))}
+    </div>
+  );
+};
+
+export default MatchingSeminars;
