49509581e3e24665c9202baecbd9683dca7ff866
Dilan Kavishka <dkgamage01@gmail.com>
2024-03-12 13:49:35 +0530

    updated component on overview bcg

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -149,12 +149,13 @@ import OrgSignUP from "./components/OrgSignUP";
 import Loading from './components/Loading';
 import SignUp from "./components/Profiles/ProfSelectSignUp";
 import UserHeader from "./components/navbar/UserHeader";
-
+import OverviewBackground from "./pages/OverviewBackground";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route index element={<Home />} />
+      {/* <Route index element={<Home />} /> */}
+      <Route index element={<OverviewBackground />} />
 
       <Route path="/past-events" element={<div>Past Events</div>} />
 
