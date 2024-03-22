6f52bb5efbf5cc6dbc338d770243cdef8990a823
Chathurdha Divyangi <chathurdhadivyangi@gmail.com>
2024-03-23 03:15:30 +0530

    Added success message to form.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -135,18 +135,18 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import { ClerkProvider, SignedIn } from "@clerk/clerk-react";
+//import { ClerkProvider, SignedIn } from "@clerk/clerk-react";
 
 import Home from "./pages/CommanPages/Homepage";
 import Login from "./components/Login";
-import Next from "./components/SignUP";
+// import Next from "./components/SignUP";
 import PastEvents from "./pages/CommanPages/PastEvents";
 import ContactUs from "./pages/CommanPages/ContactUs";
 import OrgOwnerCreate from "./components/OrgOwner-Create";
 import VolSignUP from "./components/VolSignUP";
 import SclSignUP from "./components/SclSignUP";
 import OrgSignUP from "./components/OrgSignUP";
-import PrevSem from "./pages/OrganizationPges/PrevSeminar";
+//import PrevSem from "./pages/OrganizationPges/PrevSeminar";
 import PrevSeminar from "./pages/OrganizationPges/PrevSeminar";
 import UpSeminar from "./pages/OrganizationPges/UpSeminar";
 import OrgSeminar from "./components/Organization/section/OrgSeminar";
@@ -154,11 +154,12 @@ import Seminar from "./components/Profiles/SeminarBtns";
 import OrgOverviewPg from "./pages/OrganizationPges/OrgOverviewPg";
 import OrgReceivedSeminarRequests from "./pages/OrganizationPges/ReceivedSeminarRequests";
 import OrgReceivedVolunteerRequests from "./pages/OrganizationPges/ReceivedVolunteerRequests";
+import UserHeader from "./components/Header/OrgHeader";
 import Statistics from "./pages/OrganizationPges/Statistics";
 
 import Loading from "./components/Loading";
 import SignUp from "./components/Profiles/ProfSelectSignUp";
-import UserHeader from "./components/Header/OrgHeader";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -172,7 +173,7 @@ const router = createBrowserRouter(
       <Route path="/about-us" element={<div>About Us</div>} />
 
       {/* <Route path="/Sign-In" element={<Login />} /> */}
-      <Route path="/Sign-In" element={<UserHeader />} />
+      
 
       <Route path="/Sign-Up" element={<SignUp />} />
 
@@ -195,6 +196,7 @@ const router = createBrowserRouter(
       <Route path="/UpSeminar" element={<UpSeminar />} />
       <Route path="/OrgOverview" element={<OrgOverviewPg />} />
       <Route path="/OrgRecSeminar" element={<OrgReceivedSeminarRequests />} />
+      <Route path="/Sign-In" element={<UserHeader />} />
       <Route
         path="/OrgRecVolunteer"
         element={<OrgReceivedVolunteerRequests />}
@@ -213,3 +215,42 @@ function App() {
 }
 
 export default App;
+
+// App.js
+
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { ClerkProvider } from "@clerk/clerk-react";
+// import RootLayout from "./RootLayout";
+// import { useNavigate } from "react-router-dom";
+
+// //common pages
+// import Home from "./pages/CommanPages/Homepage";
+// import PastEvents from "./pages/CommanPages/PastEvents";
+// import ContactUs from "./pages/CommanPages/ContactUs";
+
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+// if (!PUBLISHABLE_KEY) {
+//   throw new Error("Missing Publishable Key");
+// }
+
+// function App() {
+//   const navigate = useNavigate();
+//   return (
+//     <ClerkProvider navigate={navigate} publishableKey={PUBLISHABLE_KEY}>
+//       <Router>
+//         <RootLayout>
+//           <Routes>
+//             <Route index element={<Home />} />
+//             <Route path="/past-events" element={<PastEvents />} />
+//             <Route path="/contact-us" element={<ContactUs />} />
+//             <Route path="/about-us" element={<div>About Us</div>} />
+//           </Routes>
+//         </RootLayout>
+//       </Router>
+//     </ClerkProvider>
+//   );
+// }
+
+// export default App;
--- Frontend/src/components/RequestForm/DatePickerInput.jsx
+++ Frontend/src/components/RequestForm/DatePickerInput.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
+import React from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-const DatePickerInput = ({ 
-    selected, 
-    onChange, 
-    placeholderText,
-    error
-}) => { 
-    return (
-        <div className="mb-2">
-            <div className="input-field md:w-full px-3 flex flex-col justify-start">
-              <DatePicker
-                selected={selected}
-                onChange={onChange}
-                placeholderText={placeholderText}
-                className='appearance-none w-full block bg-gray-lighter text-gray-darker border border-gray-lighter rounded py-3 px-4'
-              />
-              {error && (
-                <span className="text-red-500 px-4 pt-1 text-sm flex items-center justify-start">{error}</span>
-              )}
-            </div>
-        </div>
-    );
-}
+const DatePickerInput = ({ selected, onChange, placeholderText, error }) => {
+  return (
+    <div className="mb-2">
+      <div className="input-field md:w-full px-3 flex flex-col justify-start">
+        <DatePicker
+          selected={selected}
+          onChange={onChange}
+          placeholderText={placeholderText}
+          className="appearance-none w-full block bg-gray-lighter text-gray-darker border border-gray-lighter rounded py-3 px-4"
+        />
+        {error && (
+          <span className="text-red-500 px-4 pt-1 text-sm flex items-center justify-start">
+            {error}
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default DatePickerInput;
