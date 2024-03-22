34415074d355eeeab4c4b2d1b1d72396885e7d4d
IsuruGimhana <145018121+IsuruGimhana@users.noreply.github.com>
2024-03-22 13:02:06 +0530

    filter seminars and filter volunteers updated

--- Frontend/src/components/ReceivedVolunteerRequests/FilterVolunteers.jsx
+++ Frontend/src/components/ReceivedVolunteerRequests/FilterVolunteers.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 import VolunteerAddress from './VolunteerAddress';
 import VolunteerDetails from './VolunteerDetails';
 import VolunteerHeader from './VolunteerHeader';
