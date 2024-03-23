5ece38512d1850fc87095a4bcd846e5bacba405b
IsuruGimhana <145018121+IsuruGimhana@users.noreply.github.com>
2024-03-23 19:16:02 +0530

    process date component for past events page created

---
+++ Frontend/src/components/Common/PastE-Sections/ProcessDate.jsx
@@ -0,0 +1,18 @@
+const ProcessDate = (seminar) => {
+
+    const date = seminar.createdAt ? new Date(seminar.createdAt) : null;
+
+    // Format date (using padStart for consistent day/month length)
+    const formattedDate = date?.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        })
+        .split('/')
+        //   .reverse()
+        .join('.') || 'N/A';
+
+    return { formattedDate };
+}
+ 
+export default ProcessDate;
