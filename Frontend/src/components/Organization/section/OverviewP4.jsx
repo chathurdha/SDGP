e5b545734990becdfdd7327d7ea34ae61c06ebba
IsuruGimhana <145018121+IsuruGimhana@users.noreply.github.com>
2024-03-23 19:16:30 +0530

    Organization overview updated

--- Frontend/src/components/Organization/section/OverviewP4.jsx
+++ Frontend/src/components/Organization/section/OverviewP4.jsx
@@ -1,53 +1,50 @@
 /* eslint-disable no-irregular-whitespace */
 
-
-
 import { useState, useEffect } from "react";
 import axios from "axios";
 import MatchingSeminars from "../../Common/PastE-Sections/MatchingSeminars";
 import { Link } from "react-router-dom";
 
 const OverviewP4 = () => {
-  const [pastEvents, setPastEvents] = useState([]);
+    const [pastEvents, setPastEvents] = useState([]);
+    // const [seminars, setSeminars] = useState([]);
 
-  useEffect(() => {
-    const fetchPastEvents = async () => {
-      try {
-        // Replace 'orgId' with the ID of the user's organization
-        const response = await axios.get(
-          `http://localhost:4000/api/Seminar`
-        );
-        setPastEvents(response.data);
-      } catch (error) {
-        console.error("Error fetching past events:", error);
-      }
-    };
+    useEffect(() => {
+        const fetchPastEvents = async () => {
+            try {
+                const response = await axios.get(`http://localhost:4000/api/seminars`);
+                setPastEvents(response.data);
+                console.log(response.data);
+            } catch (error) {
+                console.error("Error fetching past events:", error);
+            }
+        };
 
-    fetchPastEvents();
-  }, []);
+        fetchPastEvents();
+    }, []);
 
-  const firstThreeEvents = pastEvents.slice(0, 3);
-
-  return (
-    <>
-      <div className="flex justify-center h-auto mb-[4%]">
-        <div className="w-[70%]">
-          <div className=" relative">
-            <h1 className="text-2xl py-8 text-center font-medium">Seminars</h1>
-            <Link
-              to="/UpSeminar" // Adjust the link as per your application
-              className="underline text-right pr-4 pb-2 md:block text-custom-lightb hover:text-gray-300 text-sm font-saira"
-              style={{ fontFamily: "Saira" }}
-            >
-              See more
-            </Link>
-            <MatchingSeminars seminars={firstThreeEvents} />
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+    const firstThreeEvents = pastEvents.slice(0, 3);
+    
+    return (
+        <>
+            <div className="flex justify-center h-auto mb-[4%]">
+                <div className="w-[70%]">
+                    <div className=" relative">
+                        <h1 className="text-2xl py-8 text-center font-medium">Seminars</h1>
+                        <Link
+                            to="/UpSeminar" // Adjust the link as per your application
+                            className="underline text-right pr-4 pb-2 md:block text-custom-lightb hover:text-gray-300 text-sm font-saira"
+                            style={{ fontFamily: "Saira" }}
+                        >
+                            See more
+                        </Link>
+                        <MatchingSeminars seminars={firstThreeEvents} />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
 
 export default OverviewP4;
 
