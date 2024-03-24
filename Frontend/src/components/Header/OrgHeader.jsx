0f9d16f2917cb1b4c946c204e26db7cdfa228ae2
Imeth Pathirana <55105553+ImethP2@users.noreply.github.com>
2024-03-24 10:02:44 +0530

    fixed bugs in headers

--- Frontend/src/components/Header/OrgHeader.jsx
+++ Frontend/src/components/Header/OrgHeader.jsx
@@ -1,39 +1,14 @@
 import { useState, useEffect } from "react";
 //import { Link, useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
-// import {
-//   ClerkProvider,
-//   SignedIn,
-//   SignedOut,
-//   UserButton,
-// } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignOutButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const OrgHeader = () => {
-//  const location = useLocation(); // Get the current location
-  const [profilePic, setProfilePic] = useState(""); // State for organization profile picture
 
-  // Sample organization data (replace with your actual data)
-  const organizationData = [
-    {
-      id: 1,
-      profilePic: "/src/assets/profile1.jpg",
-    },
-    // Add more organization objects as needed
-  ];
-
-  // Function to fetch organization profile picture based on ID
-  const fetchProfilePic = (id) => {
-    // Replace this with actual logic to fetch organization profile picture
-    const org = organizationData.find((org) => org.id === id);
-    if (org) {
-      setProfilePic(org.profilePic);
-    }
-  };
-
-  useEffect(() => {
-    // Fetch organization profile picture when component mounts
-    fetchProfilePic(1); // Replace 1 with the actual organization ID
-  }, []);
 
   return (
     <header className="bg-white py-3 px-4 fixed w-screen z-10 shadow-md">
@@ -54,20 +29,18 @@ const OrgHeader = () => {
 
         {/* Organization profile picture and sign-out button */}
         <div className="flex items-center space-x-4">
-          <img
-            src={profilePic}
-            alt="Profile"
-            className="h-10 w-10 rounded-full"
-          />{" "}
+          <UserButton />
           {/* Organization profile picture */}
-          <Link
-            to="/"
-            className="bg-custom-purple hover:bg-white text-white hover:text-custom-purple hover:border border-custom-purple py-2 px-3 rounded text-sm font-saira"
-          >
-            Sign out
-          </Link>
+          <SignOutButton >
+            <Link
+              to="/"
+              className="bg-custom-purple hover:bg-white text-white hover:text-custom-purple hover:border border-custom-purple py-2 px-3 rounded text-sm font-saira"
+            >
+              Sign out
+            </Link>
+          </ SignOutButton>
         </div>
-      </div>
+      </div>            
     </header>
   );
 };
